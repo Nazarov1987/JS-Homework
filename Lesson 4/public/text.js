7df40cff054f938195944af7208e2bad1c3fe401
Nazarov1987 <ns-nazarov@mail.ru>
2019-03-29 13:45:41 +0300

    Домашнее задание №4. Реализована замена одинарных ковычек на двойные.

---
+++ Lesson 4/public/text.js
@@ -0,0 +1,20 @@
+    function sendRequest(url) {
+        return fetch(url).then((response) => response.json());
+    }
+
+    const $buttonAddText = document.querySelector('#addText');
+    $buttonAddText.addEventListener('click', () => {
+        sendRequest('http://localhost:3000/phrase'). then((phrase) => {
+        this.text = phrase.map(item => `<p> ${item.text} </p>`).join('');
+        document.querySelector('#text').innerHTML =  this.text;   
+	    this.filterText = this.text;
+        });
+    });
+
+    const $buttonQuotes = document.querySelector('#quotes');
+    $buttonQuotes.addEventListener('click', () => {	
+	this.textTwo = this.filterText.replace(/\'/g, '"');   
+    document.querySelector('#text').innerHTML = this.textTwo;
+	});
+
+
